80a894b8c8225017e8b1daad7df39b61bb4e9dcf
sandeepk275 <sandeep.kumar41991@gmail.com>
2021-12-06 15:43:07 +0530

    checkpoint 13 final

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,52 @@ function onclickView(){
    
 }
 
+
+
+
+
+let citytyping=(event)=>{
+
+ let cityvalue=document.getElementById('search-input').value;
+if(cityvalue.length>=3){
+const data = null;
+const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+if (this.readyState === this.DONE) {
+ let resobj=JSON.parse(this.responseText);
+ let res=resobj.data[0].result_object.name;
+document.getElementById("auto-items").value=res;
+document.getElementById("auto-items").style.visibility="visible";
+console.log(document.getElementById("search-input").value);
+}
+});
+
+xhr.open("GET", `https://travel-advisor.p.rapidapi.com/locations/auto-complete?query=${cityvalue}&lang=en_US&units=km`);
+xhr.setRequestHeader("x-rapidapi-host", "travel-advisor.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "240f54d273msh2e9e32197364201p14448cjsn36de92445f72");
+
+xhr.send(data);
+}else{
+document.getElementById("auto-items").style.visibility="hidden";
+}
+
+
+}
+let setvalue=(event)=>{
+    document.getElementById("search-input").value=document.getElementById("auto-items").value;
+    console.log(document.getElementById("search-input").value);
+    document.getElementById("auto-items").style.visibility="hidden";
+    searchbyCity(event);
+}
+
+
+
+
+
 //searchinng of hotel by city name
-let searchbyCity=(value)=>{
+let searchbyCity=(event)=>{
     let searchElement=document.getElementById("search-input");
     let city=searchElement.value;
     // console.log(city);
